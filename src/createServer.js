75bce9f681c0506e05a852e32e1856f49614f676
peter@lidee.be <peter@lidee.be>
2020-02-21 16:14:27 +0100

    Setup graphql made first schema, query and mutation

---
+++ src/createServer.js
@@ -0,0 +1,21 @@
+const { GraphQLServer } = require('graphql-yoga');
+const Query = require('./resolvers/Query.js');
+const Mutation = require('./resolvers/Mutation.js');
+const db = require('./db');
+
+// create graphQLServer yoga server
+function createServer(){
+    return new GraphQLServer({
+        typeDefs: 'src/schema.graphql',
+        resolvers: {
+            Mutation,
+            Query
+        },
+        resolverValidationOptions: {
+            requireResolversForResolveType: false
+        },
+        context: req => ({ ... req, db })
+    })
+}
+
+module.exports = createServer;
