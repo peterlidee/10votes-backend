376795e192899cc81814ae6ece96e31bc93f6d95
peter@lidee.be <peter@lidee.be>
2020-11-17 14:36:44 +0100

    set some debugging

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const app = express();
 
 // add the middleware
 
-
 app.use(cookieParser());
 // middleware: decode the jwt so we can get the user ID on each request
 app.use((req, res, next) => {
@@ -30,7 +29,11 @@ app.use((req, res, next) => {
 // middleware: create a middleware that populates the user on each request
 app.use(async (req, res, next) => {
     // if they aren't logged in, skip this
-    if (!req.userId) return next();
+    if (!req.userId) {
+        console.log('not logged in')
+        return next();
+    }
+    console.log('there is a req.userId', req.userId);
     const user = await db.query.user(
         { where: { id: req.userId } },
         '{ id, permissions, email, items {id}, votes {id item { id }} }'
@@ -48,90 +51,11 @@ app.use(async (req, res, next) => {
     
     server.applyMiddleware({
         app,
-    path: '/', // keep this or it will become frontend/graphql
-    cors: corsOptions,
-})
-
+        path: '/', // keep this or it will become frontend/graphql
+        cors: corsOptions,
+    })
 
 app.listen({ port: process.env.PORT || 4000 }, () => {
     //process.env.PORT && console.log(`Our app is running on port ${ PORT }`);
     console.log(`🚀 Server ready at http://localhost:4444${server.graphqlPath}`)
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const cookieParser = require('cookie-parser');
-// const jwt = require('jsonwebtoken');
-
-// require('dotenv').config({ path: 'variables.env' });
-
-// const createServer = require('./createServer');
-// const db = require('./db');
-
-// const server = createServer();
-
-// //use express middleware to handle cookies
-// server.express.use(cookieParser());
-
-// // middleware: decode the jwt so we can get the user ID on each request
-// server.express.use((req, res, next) => {
-//     // pull the token out of the req
-//     const { token } = req.cookies;
-//     if(token){
-//         const { userId } = jwt.verify(token, process.env.APP_SECRET);
-//         // put the userID on request for further requests to access
-//         req.userId = userId;
-//     }
-//     next();
-// });
-
-
-// // middleware: create a middleware that populates the user on each request
-// server.express.use( async (req, res, next) =>  {
-//     // if they aren't logged in, skip this
-//     if(!req.userId) return next();
-//     const user = await db.query.user(
-//         { where: { id: req.userId }},
-//         '{ id, permissions, email, items {id}, votes {id item { id }} }'
-//     ).catch(error => console.log(error));
-//     req.user = user;
-//     next();
-// });
-
-// // start server
-
-// // console.log('what is process.env NODE ENV?', process.env.NODE_ENV);
-// // console.log('backend, wth is process.env', process.env)                 // TODO
-// // console.log('backend, wth is FRONTEND_URL', process.env.FRONTEND_URL) // TODO
-
-// server.start(
-// {
-//     cors:  {
-//         credentials: true,
-//         origin: process.env.FRONTEND_URL,
-//     }
-// },
-// deets => {
-//     console.log(`Server is now running on port http://localhost:${deets.port}`)
-// });
