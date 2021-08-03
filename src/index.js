5eb8a8196a1ccf42e1294031776ec64b616e3d75
peter@lidee.be <peter@lidee.be>
2021-08-03 18:13:37 +0200

    added origin console log

--- src/index.js
+++ src/index.js
@@ -55,4 +55,5 @@ server.applyMiddleware({
 
 app.listen({ port: process.env.PORT || 4000 }, () => {
     console.log(`🚀 Server ready at http://localhost:4444${server.graphqlPath}`)
+    console.log('origin',corsOptions.origin)
 });
